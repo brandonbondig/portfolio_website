3ee75bf1b32546da919d07fb87f133680470520a
Brandon Bondig <brandon@bondig.dk>
2022-09-19 10:51:06 +0200

    changed order of middleware

--- server/server.js
+++ server/server.js
@@ -14,12 +14,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "../client/dist")));
 
-// Handling non matching request from the client
-app.use((req, res, next) => {
-    res.status(404).send(
-        "<h1>Page not found</h1>")
-});
-
 // Frontend
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "../client/dist", "index.html"))
@@ -42,6 +36,13 @@ const test = require("./api/test.js");
 
 // Endpoints
 app.use("/api/test", test);
+
+// Handling non matching request from the client
+app.use((req, res, next) => {
+    res.status(404).send(
+        "<h1>Page not found</h1>")
+});
+
 // Listener
 app.listen(PORT, () => {
   console.log("Server Port: " + PORT);
