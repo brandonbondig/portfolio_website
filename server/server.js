69281cdf489097dc02686c69b6abee8ba8ceb214
Brandon Bondig <brandon@bondig.dk>
2022-09-22 17:35:17 +0200

    changes

--- server/server.js
+++ server/server.js
@@ -39,8 +39,8 @@ app.use("/api/test", test);
 
 // Handling non matching request from the client
 app.use((req, res, next) => {
-    res.status(404).send(
-        "<h1>Page not found</h1>")
+  res.status(404).send(
+    "<h1>Page not found</h1>")
 });
 
 // Listener
